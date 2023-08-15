d1690a44b1f59ee13c244bb891d235d25ed78c99
Psalm1st <113413136+Psalm1st@users.noreply.github.com>
2023-08-15 08:57:53 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+
+![Screenshot 1](img/demo.png)
+
+## Demo
+
+Live Demo - Coming Soon...
+
+## About
+
+School Children Quiz App is a simple, fun and educational web-based quiz application designed to teach kids about internet safety, cyberbullying awareness, and responsible online behavior.
+
+## Introduction
+
+Welcome to the School Children Quiz App! This interactive quiz is designed to help kids learn important internet safety concepts while having fun. The quiz covers topics such as identifying spam emails, dealing with cyberbullying, protecting personal information online, and more.
+
+## Features
+
+- Engaging quiz format with multiple-choice questions.
+- Questions focus on internet safety, cyberbullying, and responsible online behavior.
+- Timer adds excitement and challenges players to think quickly.
+- Randomized question order for replayability.
+- Colorful and user-friendly interface suitable for kids.
+
+## How to Play
+
+1. Click the "Play Quiz" button to start the quiz.
+2. Read the rules and instructions carefully.
+3. Answer each question within the given time limit.
+4. View your score and see how well you did at the end.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/Psalm1st/Diff-2.git`
+2. Open `index.html` in a web browser.
+
+## Contribution
+
+Contributions are welcome! If you find any issues or want to add new questions, feel free to create a pull request.
+
+## Credits
+
+- Samuel Chukwu - Developer
+- Quiz questions adapted from random source of knowledge
+
+## License
+
+This project is licensed under the MIT License.
---
+++ js/questions.js
@@ -0,0 +1,180 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "If you are in a gaming room and someone that you do not know asks you about personal information (same age as you-or close), what do you do?",
+    answer: "D. Both A and C",
+    options: [
+      "A. Don't ever give out any personal information.",
+      "B. Things on the internet must always be trusted.",
+      "C. No!. They could be something different than they say.",
+      "D. Both A and C"
+    ]
+  },
+  {
+    numb: 2,
+    question: "While chatting online, you receive a friend request from someone you don't know. What's the best action to take?",
+    answer: "B. Decline the friend request and let your parents or a teacher know about it.",
+    options: [
+      "A. Accept the friend request, as they might just be a new classmate.",
+      "B. Decline the friend request and let your parents or a teacher know about it.",
+      "C. Share personal details with them to get to know them better.",
+      "D. Block them immediately without thinking."
+    ]
+  },
+  {
+    numb: 3,
+    question: "What should you eliminate from photos that you post online?",
+    answer: "A. Any recognizable landmarks.",
+    options: [
+      "A. Any recognizable landmarks.",
+      "B. Nudity.",
+      "C. Faces of others in photo.",
+      "D. Any illegal activities."
+    ]
+  },
+  {
+    numb: 4,
+    question: "What should you do if you come across mean or hurtful messages online?",
+    answer: "C. Show it to a trusted adult and ask for their guidance.",
+    options: [
+      "A. Respond with an equally mean message to stand up for yourself.",
+      "B. Share it with your friends for advice on how to reply.",
+      "C. Show it to a trusted adult and ask for their guidance.",
+      "D. Ignore it and hope the sender stops."
+    ]
+  },
+  {
+    numb: 5,
+    question: "A 'boy/girl' contacts you through a chat board. He/she is exactly your age, likes the same things as you, likes the same music you do. After a few weeks of chatting, he/she asks you to meet him/her.  What do you do?",
+    answer: "C. Don't trust everything online; avoid meeting in person and tell a trusted adult about the situation.",
+  options: [
+    "A. Agree to meet them, but bring a friend along for safety.",
+    "B. Arrange the meeting at a busy public place to ensure safety.",
+    "C. Don't trust everything online; avoid meeting in person and tell a trusted adult about the situation.",
+    "D. Share personal information with them as a sign of trust."
+  ]
+  },
+  {
+    numb: 6,
+    question: "When you are using your facebook account, a message pops-out, it tells you to click on the link and you can get an iPhone if you win the game. What would you do?",
+    answer: "C. It's a lie, I will just ignore it.",
+    options: [
+      "A. Just click on the link, and I can get a free iPhone by that.",
+      "B. Just click on the link, and tell my friend to play it too.",
+      "C. It's a lie, I will just ignore it.",
+      "D. Tell my friend to try it first, and I will play"
+    ]
+  },
+  {
+    numb: 7,
+    question: "If you suspect your computer is infected with malware, what is the best course of action?",
+    answer: "C. Use anti-virus and anti-spyware software to defend against malware and seek professional help if needed.",
+    options: [
+      "A. Continue using your computer as usual; malware isn't a serious threat.",
+      "B. Turn off your computer and avoid using it for a few days.",
+      "C. Use anti-virus and anti-spyware software to defend against malware and seek professional help if needed.",
+      "D. Delete all your files and reinstall the operating system immediately."
+    ]
+  },
+  {
+    numb: 8,
+    question: "How can you identify a spam email?",
+  answer: "A. It may have 'spam' written in the subject line, or it might ask for personal information.",
+  options: [
+    "A. It may have 'spam' written in the subject line, or it might ask for personal information.",
+    "B. It usually comes from a friend or family member.",
+    "C. It contains only text without any images or links.",
+    "D. It promises free gifts or prizes without any conditions."
+  ]
+  },
+  {
+    numb: 9,
+    question: "If you know you receive a spam email, what should you do next?",
+    answer: "D. Ignore it and delete it.",
+    options: [
+      "A. Just leave it there.",
+      "B. Send this email to your friend.",
+      "C. Still use your personal account on the computer.",
+      "D. Ignore it and delete it."
+    ]
+  },
+  {
+    numb: 10,
+    question: "What should you do if you receive a message from a stranger claiming you've won a huge prize in a contest you never entered?",
+  answer: "C. Delete the message and don't respond.",
+  options: [
+    "A. Celebrate and share the news with all your friends.",
+    "B. Respond immediately with your personal information to claim the prize.",
+    "C. Delete the message and don't respond.",
+    "D. Ask the sender for more information about the contest before taking any action."
+  ]
+  },
+  {
+    numb: 11,
+    question: "If one of your best friends send you an email about help him to put some money in a game, and he never played games before, what would you do?",
+    answer: "C. Contact your friend to verify the email and make sure their account hasn't been compromised.",
+    options: [
+      "A. He just started playing game now, I can help him first.",
+      "B. Email back, ask him it's real or not.",
+      "C. Contact your friend to verify the email and make sure their account hasn't been compromised.",
+      "D. He is my best friend, I can trust him anyways."
+    ]
+  },
+  {
+    numb: 12,
+    question: "You have heard about a new social networking site but the age limit is higher than your actual age",
+    answer: "B. You wait until you're the correct age then sign up for the site.",
+    options: [
+      "A. You sign up anyways; you'll be old enough in two months anyways.",
+      "B. You wait until you're the correct age then sign up for the site.",
+      "C. You put a fake age in and don't tell your parents.",
+      "D. All your friends are members so you're entitled to be one too."
+    ]
+  },
+  {
+    numb: 13,
+    question: "It is VERY okay to cyber bully, even if you're kidding around.",
+    answer: "B. False",
+    options: [
+      "A. True.",
+      "B. False",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 14,
+    question: "What should you do if someone online asks you for your password?",
+    answer: "C. Never share your password with anyone, even if they claim to be a friend.",
+    options: [
+      "A. Share your password, but only if they promise not to misuse it.",
+      "B. Share your password, but only with people you know in real life.",
+      "C. Never share your password with anyone, even if they claim to be a friend.",
+      "D. Share your password if they offer you something valuable in return."
+    ]
+  },
+  {
+    numb: 15,
+    question: "You video your friend sleeping on her couch at home, you think it will be really funny to upload it to face book so you",
+    answer: "C. I should probably ask for her permission to upload this first otherwise she could get really upset.",
+    options: [
+      "A. Think to yourself, oh it will be so funny when I tag her in it and all her other friends see it too.",
+      "B. She will be angry to start off with but she will think its funny eventually… How can't she, it's hilarious!.",
+      "C. I should probably ask for her permission to upload this first otherwise she could get really upset.",
+      "D. Video wars here we come."
+    ]
+  },
+  {
+    numb: 16,
+    question: "At school you accidentally see a girl enter her password for internet access. You now can log on as her and read all her emails and send emails to people and they would think it was from her. You...",
+    answer: "B. Promise yourself you won't log onto her page, and tell her the need to change it.",
+    options: [
+      "A. Wait for her to leave then log straight onto her email and read all her emails and show everyone around you.",
+      "B. Promise yourself you won't log onto her page, and tell her the need to change it.",
+      "C. Share the password with your friends to prove you know it.",
+      "D. Try that exact password on her Facebook account and see if you can hack it for a laugh."
+    ]
+  },
+  
+];
